9b8280996038ba06c90e52ab3e286d38432e50d0
KAIF 0 <kaif8700979251@gmail.com>
2025-03-08 02:00:41 +0530

    commit

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+
+const server = serve({
+    port: 3000,
+    fetch(request) { 
+        return new Response("Hello via Bun!");
+    },
+}); 
+
+console.log(`Server running on http://localhost:${server.port}`);
